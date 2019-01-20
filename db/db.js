07d69614c0b1057b591857c83be6781725050a40
Shavel, Ihar <IShavel@slb.com>
2019-01-20 18:52:22 +0300

    feat: add crud operations

---
+++ db/db.js
@@ -0,0 +1,361 @@
+let news = [
+  {
+    source: {
+      id: "al-jazeera-english",
+      name: "Al Jazeera English"
+    },
+    author: "Al Jazeera",
+    title:
+      "Syrian air defence repels Israeli attack in south: State media - Aljazeera.com",
+    description:
+      "Air defence systems thwart Israeli air raid, state media reports, hours after blast rocks Damascus.",
+    url:
+      "https://www.aljazeera.com/news/2019/01/syrian-air-defence-repels-israeli-attack-south-state-media-190120112642780.html",
+    urlToImage:
+      "https://www.aljazeera.com/mritems/Images/2018/6/26/fd75641601e3406b8617b70856a901d4_18.jpg",
+    publishedAt: "2019-01-20T12:31:00Z",
+    content:
+      "The Syrian military has thwarted an Israeli attack on Sunday, shooting down several missiles in the south of the country, state media and Russia's Ministry of Defense have said. \"Our air defence systems thwarted... an Israeli air aggression... and prevented i… [+3013 chars]"
+  },
+  {
+    source: {
+      id: "politico",
+      name: "Politico"
+    },
+    author: "GABBY ORR",
+    title: "Mark Meadows, Trump whisperer - POLITICO",
+    description:
+      "The conservative lawmaker talks to Trump more than the president does with many senior aides, sometimes spending an hour-plus on the phone or speaking multiple times a day.",
+    url:
+      "https://www.politico.com/story/2019/01/20/mark-meadows-trump-shutdown-1116057",
+    urlToImage:
+      "https://static.politico.com/82/e4/aa1301a340ce98d45f59816a0473/190119-meadows-ap-773.jpg",
+    publishedAt: "2019-01-20T11:53:00Z",
+    content:
+      "Rep. Mark Meadows, a three-term North Carolina congressman little known outside the Beltway, has earned an outsize influence on shaping the direction of the Trump administration. | Jacquelyn Martin/AP Photo When Mark Meadows didnt get President Donald Trumps … [+8946 chars]"
+  },
+  {
+    source: {
+      id: "reuters",
+      name: "Reuters"
+    },
+    author: "Barbara Goldberg",
+    title:
+      "Total lunar eclipse set to wow star gazers, clear skies willing - Reuters",
+    description:
+      "Astronomy buffs across the United States have been promised all the makings of a spectacular total lunar eclipse on Sunday except one - clear skies.",
+    url:
+      "https://www.reuters.com/article/us-lunar-eclipse/total-lunar-eclipse-set-to-wow-star-gazers-clear-skies-willing-idUSKCN1PE0CM",
+    urlToImage:
+      "https://s2.reutersmedia.net/resources/r/?m=02&d=20190120&t=2&i=1347771420&w=1200&r=LYNXNPEF0J0HB",
+    publishedAt: "2019-01-20T11:45:00Z",
+    content:
+      "NEW YORK/LOS ANGELES (Reuters) - Astronomy buffs across the United States have been promised all the makings of a spectacular total lunar eclipse on Sunday except one - clear skies. Star gazers from Los Angeles to New York will keep their eyes on the sky for … [+1731 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Eonline.com"
+    },
+    author: "Natalie Finn",
+    title:
+      "The Kidnapping of Jayme Closs: Charges Lay Out a Terrifying Story - E! NEWS",
+    description:
+      "What happened to the 13-year-old, whom prosecutors say survived three months in captivity, is a nightmare and a miracle wrapped into one",
+    url:
+      "https://www.eonline.com/news/1006158/the-story-unfolding-about-jayme-closs-and-the-man-charged-with-kidnapping-her-and-killing-her-parents-is-only-getting-more-incredible",
+    urlToImage:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/2019017/rs_600x600-190117115708-600-jayme-closs2.jpg?fit=around|600:467&crop=600:467;center,top&output-quality=90",
+    publishedAt: "2019-01-20T11:00:00Z",
+    content:
+      "Fitzgerald said at a news conference on Oct. 24 that more than 2,000 people did indeed turn out to help. \"Jayme, not a moment goes by when we aren't thinking of you, and praying for you,\" the child's aunt Jennifer Smith, Denise Closs' sister, read a statement… [+879 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Latimes.com"
+    },
+    author: "Doyle McManus",
+    title:
+      "Nancy Pelosi is winning in her battle with Trump. Here's why - Los Angeles Times",
+    description:
+      "This is the showdown Nancy Pelosi has been planning for, preparing for, even yearning for since President Trump was inaugurated two years ago. Trump is learning a lesson many others have learned the hard way: Pelosi may be a San Francisco progressive but ther…",
+    url:
+      "https://www.latimes.com/politics/la-na-pol-mcmanus-column-pelosi-20190120-story.html",
+    urlToImage:
+      "https://www.latimes.com/resizer/I5qUlG4BpLQqO15MuW83MYvYlWM=/1200x0/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/IBC6ZT46SNFU7IFGQKRQFHS6N4.jpg",
+    publishedAt: "2019-01-20T11:00:00Z",
+    content:
+      "The House speaker is fond of three precepts; spend time with her and you’ll hear them all. One is from Abraham Lincoln: “Public sentiment is everything. With it, nothing can fail; against it, nothing can succeed.” The second is from her father, an old-school … [+4493 chars]"
+  },
+  {
+    source: {
+      id: "nbc-news",
+      name: "NBC News"
+    },
+    author: "James Rainey",
+    title:
+      "Fire turns Chico, California, into a boomtown, but at what cost? - NBC News",
+    description:
+      "Chico, California, has seen its population jump as much as 20,000, traffic accidents climb 50 percent and some businesses boom, in what is now America's hottest housing market.",
+    url:
+      "https://www.nbcnews.com/news/us-news/fire-turns-chico-california-boomtown-what-cost-n960081",
+    urlToImage:
+      "https://media1.s-nbcnews.com/j/newscms/2019_03/2722521/190119-peter-dasilva-chico-tin-roof-ew-1123a_b341283724b6ef3980bb6a778866399d.nbcnews-fp-1200-630.jpg",
+    publishedAt: "2019-01-20T10:35:00Z",
+    content:
+      "Now, the population has spiked by 10,000 to 20,000 because of people who fled the fire, not to mention hundreds of repair and restoration workers from state and federal agencies and from the Pacific Gas and Electric utility. That has pushed the population of … [+5264 chars]"
+  },
+  {
+    source: {
+      id: "newsweek",
+      name: "Newsweek"
+    },
+    author: "Tom Porter",
+    title:
+      "MAGA Hat Wearing Pupils From Covington Catholic School Who Taunted Native American Elder Could Face Expulsion - Newsweek",
+    description:
+      "Videos show the youths surrounding Omaha elder Nathan Phillips.",
+    url:
+      "https://www.newsweek.com/maga-cap-wearing-pupils-catholic-school-who-taunted-native-americans-could-1298460",
+    urlToImage:
+      "https://s.newsweek.com/sites/www.newsweek.com/files/styles/full/public/2019/01/20/gettyimages-1084652098.jpg",
+    publishedAt: "2019-01-20T10:28:26Z",
+    content:
+      "Pupils from a Catholic boys school in Kentucky could face disciplinary action after they were filmed surrounding and taunting a group of Native American protesters in Washington DC on Saturday. The Diocese of Covington in a statement on Saturday issued an apo… [+2267 chars]"
+  },
+  {
+    source: {
+      id: "the-huffington-post",
+      name: "The Huffington Post"
+    },
+    author: "Amy Russo",
+    title:
+      "'Everybody's BS Alarm Should've Been Blinking Red' On BuzzFeed Scoop: Michael Smerconish - HuffPost",
+    description: "“It didn’t pass the smell test!”",
+    url:
+      "https://www.huffingtonpost.com/entry/michael-smerconish-buzzfeed_us_5c442070e4b0bfa693c443bf",
+    urlToImage:
+      "https://img.huffingtonpost.com/asset/5c4435412500002c017db65c.jpeg?cache=zoHsJE1zdr&ops=1200_630",
+    publishedAt: "2019-01-20T10:27:00Z",
+    content:
+      "In the fallout of a disputed BuzzFeed story alleging President Donald Trump directed Michael Cohen to deceive Congress on the Moscow project, CNN’s Michael Smerconish said he smelled something fishy from the start. BuzzFeed’s report Thursday, which cited two … [+1836 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Espn.com"
+    },
+    author: null,
+    title:
+      "Australian Open - Experts - Breaking down the Serena Williams-Simona Halep clash Down Under - ESPN",
+    description:
+      "Who has the advantage in the Australian Open quarterfinals? Serena Williams or Simona Halep? We asked a panel of experts.",
+    url:
+      "http://www.espn.com/tennis/story/_/id/25807576/australian-open-experts-breaking-serena-williams-simona-halep-clash-under",
+    urlToImage:
+      "https://a4.espncdn.com/combiner/i?img=%2Fphoto%2F2019%2F0120%2Fr491224_1296x729_16%2D9.jpg",
+    publishedAt: "2019-01-20T10:20:12Z",
+    content:
+      "MELBOURNE, Australia -- We'd be lying if we said this match wasn't one of the first significant showdowns on our radar the moment the Australian Open draw was released. Turns out the results have played out as we had hoped, and now it's on: Serena Williams ve… [+9336 chars]"
+  },
+  {
+    source: {
+      id: "nbc-news",
+      name: "NBC News"
+    },
+    author: "Associated Press",
+    title:
+      "Around 120 migrants feared dead after overloaded dingy sinks in Mediterranean - NBC News",
+    description:
+      "Three survivors of a rubber dinghy that sank in the Mediterranean Sea off the coast of Libya say up to 117 other migrants were aboard at the time.",
+    url:
+      "https://www.nbcnews.com/news/world/117-missing-sunken-migrant-boat-libya-survivors-say-n960666",
+    urlToImage:
+      "https://media4.s-nbcnews.com/j/newscms/2019_03/2723006/afp_1ce80n_1_b2ba15191881fc53c2d54f03eb0da639.nbcnews-fp-1200-630.jpg",
+    publishedAt: "2019-01-20T10:03:00Z",
+    content:
+      "Get breaking news alerts and special reports. The news and stories that matter, delivered weekday mornings. SUBSCRIBE Jan. 20, 2019, 10:03 AM GMT ROME Three survivors of a rubber dinghy that sank in the Mediterranean Sea off the coast of Libya say up to 117 o… [+3778 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Avclub.com"
+    },
+    author: "Dennis Perkins",
+    title:
+      "Rachel Brosnahan makes the most of a not-quite-marvelous Saturday Night Live - The A.V. Club",
+    description: "“Maybe you can tweet about it, that will\nsolve everything.”",
+    url:
+      "https://tv.avclub.com/rachel-brosnahan-makes-the-most-of-a-not-quite-marvelou-1831901562",
+    urlToImage:
+      "https://i.kinja-img.com/gawker-media/image/upload/s--oe9UbAC6--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/tonzljhcqbrtfwqwwrxk.png",
+    publishedAt: "2019-01-20T09:33:00Z",
+    content:
+      "Maybe you can tweet about it, that will solve everything. Im not an actor, Im a [streaming] star! Rachel Brosnahan has rightly broken out as the brashly funny stand-up comic star of The Marvelous Mrs. Maisel, so its strange how little SNL used her in her firs… [+14870 chars]"
+  },
+  {
+    source: {
+      id: "cnn",
+      name: "CNN"
+    },
+    author: "Leona Siaw, Hira Humayun and Susannah Cullinane, CNN",
+    title:
+      "Congo presidential runner-up rejects court ruling and declares himself president-elect - CNN",
+    description:
+      "Democratic Republic of Congo opposition leader Martin Fayulu rejected a court ruling Saturday that upheld his defeat in the country's presidential election, instead declaring himself president-elect.",
+    url:
+      "https://www.cnn.com/2019/01/20/africa/dr-congo-martin-fayulu-rejects-court-ruling/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/190111123003-01-martin-fayulu-0110-super-tease.jpg",
+    publishedAt: "2019-01-20T08:41:00Z",
+    content: null
+  },
+  {
+    source: {
+      id: null,
+      name: "Mmajunkie.com"
+    },
+    author: "Steven Marrocco",
+    title:
+      "Greg Hardy denies illegal knee in UFC debut was on purpose: 'Go ask Tom Brady if I cheated' - MMAjunkie",
+    description:
+      "Greg Hardy was brought to tears defending himself after getting disqualified at UFC on ESPN+ 1.",
+    url:
+      "https://mmajunkie.com/2019/01/ufc-brooklyn-greg-hardy-denies-illegal-knee-intentional-disqualification",
+    urlToImage:
+      "https://usatmmajunkie.files.wordpress.com/2019/01/greg-hardy-ufc-brooklyn-post-fight-4.jpg?w=1000&h=576&crop=1",
+    publishedAt: "2019-01-20T08:40:00Z",
+    content:
+      "BROOKLYN, N.Y. Former NFL All-Pro turned UFC heavyweight Greg Hardy said his illegal knee against Allen Crowder came out of inexperience and denied intentionally throwing the disqualifying strike. “I take full responsbility,” Hardy, responding to MMAjunkie, t… [+3671 chars]"
+  },
+  {
+    source: {
+      id: "fox-news",
+      name: "Fox News"
+    },
+    author: "Louis Casiano",
+    title:
+      "Pipeline explosion witnesses describe scene where 73 died: 'People's skin came off' - Fox News",
+    description:
+      "Grief-stricken family members and witnesses to Friday's pipeline explosion in central Mexico -- which killed at least 73 people, burning many to just bones or ash, and injured at least 74 -- were still reeling from the tragedy Saturday.",
+    url:
+      "https://www.foxnews.com/world/death-toll-from-mexico-pipeline-explosion-rises-to-73-dozens-injured",
+    urlToImage:
+      "https://static.foxnews.com/foxnews.com/content/uploads/2019/01/AP19019518578952.jpg",
+    publishedAt: "2019-01-20T08:13:09Z",
+    content:
+      "Grief-stricken family members and witnesses to Friday's pipeline explosion in central Mexico -- which killed at least 73 people, burning many to just bones or ash, and injured at least 74 -- were still reeling from the tragedy Saturday. “Some people’s skin ca… [+4894 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Variety.com"
+    },
+    author: "Kristopher Tapley, Kristopher Tapley",
+    title:
+      "Oscars: With PGA Victory, ‘Green Book’ Becomes Best Picture Frontrunner - Variety",
+    description:
+      "Save for a pair of recent back-to-back discrepancies in “The Big Short” and “La La Land,” the Producers Guild’s Darryl F. Zanuck Award for Theatrical Motion Pictures h…",
+    url:
+      "https://variety.com/2019/film/in-contention/oscars-with-pga-victory-green-book-becomes-best-picture-frontrunner-1203112994/",
+    urlToImage:
+      "https://pmcvariety.files.wordpress.com/2019/01/green.jpg?w=700&h=393&crop=1",
+    publishedAt: "2019-01-20T07:47:00Z",
+    content:
+      "Save for a pair of recent back-to-back discrepancies in “The Big Short” and “La La Land,” the Producers Guild’s Darryl F. Zanuck Award for Theatrical Motion Pictures has been a fairly reliable barometer for the annual Oscar season outcome. At least, ever sinc… [+2844 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Thedrive.com"
+    },
+    author: "Chris Tsui",
+    title:
+      "First-Ever 2020 Toyota Supra Just Sold for a Whopping $2.1 Million - The Drive",
+    description:
+      "All proceeds are going to the American Heart Association and the Bob Woodruff Foundation.",
+    url:
+      "http://www.thedrive.com/news/26068/first-ever-2020-toyota-supra-just-sold-for-a-whopping-2-1-million",
+    urlToImage:
+      "https://timedotcom.files.wordpress.com/2019/01/dxuzzdnvsaaa03r.jpg?quality=85",
+    publishedAt: "2019-01-20T04:06:42Z",
+    content:
+      "When Toyota announced that it would auction off the very first 2020 Toyota Supra for charity, we knew it would pull in big bucks, and boy, we weren't wrong. Finding its very first owner at the Barrett-Jackson Auction in Scottsdale, Arizona on Saturday night, … [+2036 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Japantimes.co.jp"
+    },
+    author: null,
+    title:
+      "France presses Japan to accept Renault-Nissan merger - The Japan Times",
+    description:
+      "The French government has demanded Japan accept a possible merger between Renault SA and Nissan Motor Co. following the arrest of Carlos Ghosn, who had man",
+    url:
+      "https://www.japantimes.co.jp/news/2019/01/20/business/corporate-business/france-presses-japan-accept-renault-nissan-merger/",
+    urlToImage:
+      "https://cdn.japantimes.2xx.jp/wp-content/uploads/2019/01/b-nissan-a-20190121-870x576.jpg",
+    publishedAt: "2019-01-20T03:48:00Z",
+    content:
+      "The French government has demanded Japan accept a possible merger between Renault SA and Nissan Motor Co. following the arrest of Carlos Ghosn, who had managed the alliance of the carmakers, sources close to the matter said Sunday. The request, which came dur… [+2193 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Si.com"
+    },
+    author: "Albert Breer",
+    title:
+      "How Sean McVay Steals Plays From Sean Payton and Vice Versa—and Why It’s the Ultimate Sign of Respect - Sports Illustrated",
+    description:
+      "In November, Payton detailed how he watches Rams tape closely and takes some of their concepts. McVay admits to doing the same thing with Payton's offense, and even stole a play that burned the Rams defense to use one week later. Plus, news and notes for the …",
+    url:
+      "https://www.si.com/nfl/2019/01/19/sean-mcvay-sean-payton-stealing-plays-nfc-title-game-news-notes-chiefs-patriots-afc-title-game-eric",
+    urlToImage:
+      "https://imagesvc.timeincapp.com/v3/mm/image?url=https://cdn-s3.si.com/s3fs-public/2019/01/19/sean-payton-sean-mcvay.jpg&w=1200&h=628&c=sc&poi=face&q=85",
+    publishedAt: "2019-01-20T03:32:29Z",
+    content:
+      "If the Seahawks had taken a real hard look at the Rams previous game tape before their Week 10 visit to L.A., theyd have seen the concept that shook slot receiver Cooper Kupp free for a big gainer at the end of the first quarter against them. But its hard to … [+11617 chars]"
+  },
+  {
+    source: {
+      id: null,
+      name: "Dawgnation.com"
+    },
+    author: "Mike Griffith",
+    title:
+      "Georgia WR Terry Godwin wins offensive MVP in East-West Shrine Game - DawgNation",
+    description:
+      "ATHENS — Terry Godwin capped his collegiate career with an eye-opening performance at Tropicana Field in the East-West Shrine Game. Godwin, a Georgia football fan favorite, had four catches for...",
+    url:
+      "https://www.dawgnation.com/football/georgia-godwin-uga-dawgs-fbc-shrine",
+    urlToImage:
+      "https://res.cloudinary.com/dawgnation/image/upload/c_crop,g_north_west,h_1265,w_2400,x_0,y_143/f_auto,q_auto/h_624,w_1184/v1541292336/Terry-Godwin-by-Curtis-Compton-AJC-110418-uga-photos-CC31_j3ovdb.jpg",
+    publishedAt: "2019-01-20T02:14:35Z",
+    content:
+      "ATHENS Terry Godwin capped his collegiate career with an eye-opening performance at Tropicana Field in the East-West Shrine Game. Godwin, a Georgia football fan favorite, had four catches for 80 yards and 2 touchdowns and was named the offensive MVP on Saturd… [+1291 chars]"
+  },
+  {
+    source: {
+      id: "cbs-news",
+      name: "CBS News"
+    },
+    author: "CBS/AP",
+    title: "Dangerous winter storm disrupts travel across U.S. - CBS News",
+    description:
+      "The storm is expected to drop snow, sleet, freezing rain and ice in many places",
+    url:
+      "https://www.cbsnews.com/live-news/winter-storm-haper-2019-state-emergency-snow-storm-warning-latest-weather-forecast-updates-today/",
+    urlToImage:
+      "https://cbsnews2.cbsistatic.com/hub/i/r/2019/01/20/fa44fa5f-d768-497a-a39d-3992f6a47866/thumbnail/1200x630/3f71431524ff6f2450576a93be17857a/ap-19019847228673.jpg",
+    publishedAt: "2019-01-20T00:07:48Z",
+    content:
+      "Updated on: January 19, 2019 / 8:08 PM / CBS/AP Dangerous winter weather slams U.S. A large, powerful and dangerous winter storm is taking aim at about one-third of the nation this weekend. It rapidly moved from the Central Plains to the Midwest and is moving… [+6006 chars]"
+  }
+];
+
+module.exports = {
+  news
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-const constants = require("./constants")
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.all("*", function(request, response){
-    response.send(constants.SAMPLE_RESPONSE);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let db = require("../db/db")
+
+app.get("/news", function(request, response){
+    console.log("GET request");
+    
+    response.send(db.news);
+});
+
+app.get("/news/:id", function(request, response){
+    const id = request.params.id;
+
+    console.log("GET request with parameter id = " + id);
+
+    response.send(db.news.id);
+});
+
+app.put("/news/:id", function(request, response){
+    const id = request.params.id;
+
+    console.log("GET request with parameter id = " + id);
+
+    db.news.id = request.body
+
+    response.send(db.news.id);
+});
+
+app.post("/news", function(request, response){
+    console.log("POST request");
+    console.log("body", request.body);
+
+    db.news = request.body
+
+    response.send(db.news);
+});
+
+app.delete("/news/:id", function(request, response){
+    const id = request.params.id;   
+
+    db.news = db.news.filter(function(value, index){
+        return index != id - 1;
+    });
+
+    console.log("DELETE request with parameter id = " + id);
+
+    response.send(db.news);
 });
 
 app.listen(3000);
