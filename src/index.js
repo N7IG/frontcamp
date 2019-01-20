780eae89351b25bed56f546308b11eb31d149542
Shavel, Ihar <IShavel@slb.com>
2019-01-20 20:28:55 +0300

    feat: add error handling

--- src/index.js
+++ src/index.js
@@ -1,55 +1,86 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require('fs');
 const app = express();
+const DB_PATH = "./database/db.json";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let db = require("../db/db")
-
 app.get("/news", function(request, response){
     console.log("GET request");
-    
-    response.send(db.news);
+    fs.readFile(DB_PATH, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        response.send(data);
+    })
 });
 
 app.get("/news/:id", function(request, response){
     const id = request.params.id;
-
     console.log("GET request with parameter id = " + id);
 
-    response.send(db.news.id);
+    fs.readFile(DB_PATH, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const dataObject = JSON.parse(data);        
+        response.send(JSON.stringify(dataObject[id]));
+    })
 });
 
 app.put("/news/:id", function(request, response){
     const id = request.params.id;
-
     console.log("GET request with parameter id = " + id);
 
-    db.news.id = request.body
-
-    response.send(db.news.id);
+    fs.readFile(DB_PATH, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const dataObject = JSON.parse(data);  
+        dataObject[id] = request.body;
+        fs.writeFile(DB_PATH, JSON.stringify(dataObject), (err, data, next) => {
+            if (err) {
+                next(err);
+            } else { 
+                response.send(dataObject);
+            }
+        })
+    })
 });
 
 app.post("/news", function(request, response){
     console.log("POST request");
     console.log("body", request.body);
 
-    db.news = request.body
-
-    response.send(db.news);
+    fs.writeFile(DB_PATH, JSON.stringify(request.body), (err, data, next) => {
+        if (err) {
+            next(err);
+        } else { 
+            response.send(request.body);
+        }
+    })
 });
 
 app.delete("/news/:id", function(request, response){
-    const id = request.params.id;   
+    const id = request.params.id;  
+    
+    fs.readFile(DB_PATH, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const dataObject = JSON.parse(data);  
+        delete dataObject[id];
 
-    db.news = db.news.filter(function(value, index){
-        return index != id - 1;
-    });
-
-    console.log("DELETE request with parameter id = " + id);
-
-    response.send(db.news);
+        fs.writeFile(DB_PATH, JSON.stringify(dataObject), (err, data, next) => {
+            if (err) {
+                next(err);
+            } else { 
+                response.send(dataObject);
+            }
+        })
+    })
 });
 
 app.listen(3000);
