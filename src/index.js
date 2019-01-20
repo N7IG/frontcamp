38024df8f4699389c31f703a3afac3d5e6cd6ccf
Shavel, Ihar <IShavel@slb.com>
2019-01-20 20:57:26 +0300

    feat: add winston logger

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ const bodyParser = require("body-parser");
 const fs = require('fs');
 const app = express();
 const DB_PATH = "./database/db.json";
+const filename = "./results.log";
+const { createLogger, format, transports } = require('winston');
+const logger =  createLogger({
+    level: 'info',
+    format: format.combine(
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      format.json()
+    ),
+    transports: [
+      new transports.Console({
+        level: 'info',
+        format: format.combine(
+          format.colorize(),
+          format.printf(
+            info => `${info.timestamp} ${info.level}: ${info.message}`
+          )
+        )
+      }),
+      new transports.File({ filename })
+    ]
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/news", function(request, response){
-    console.log("GET request");
+    logger.info("URL: '/news', GET request");
     fs.readFile(DB_PATH, 'utf8', (err, data) => {
         if (err) {
             throw err;
@@ -19,7 +42,7 @@ app.get("/news", function(request, response){
 
 app.get("/news/:id", function(request, response){
     const id = request.params.id;
-    console.log("GET request with parameter id = " + id);
+    logger.info(`URL: '/news${id}', GET request with parameter id = ${id}`);
 
     fs.readFile(DB_PATH, 'utf8', (err, data) => {
         if (err) {
@@ -32,7 +55,7 @@ app.get("/news/:id", function(request, response){
 
 app.put("/news/:id", function(request, response){
     const id = request.params.id;
-    console.log("GET request with parameter id = " + id);
+    logger.info(`URL: '/news${id}', PUT request with parameter id = ${id}`);
 
     fs.readFile(DB_PATH, 'utf8', (err, data) => {
         if (err) {
@@ -51,8 +74,7 @@ app.put("/news/:id", function(request, response){
 });
 
 app.post("/news", function(request, response){
-    console.log("POST request");
-    console.log("body", request.body);
+    logger.info(`URL: '/news', PUT request`);
 
     fs.writeFile(DB_PATH, JSON.stringify(request.body), (err, data, next) => {
         if (err) {
@@ -65,6 +87,8 @@ app.post("/news", function(request, response){
 
 app.delete("/news/:id", function(request, response){
     const id = request.params.id;  
+
+    logger.info(`URL: '/news${id}', DELETE request with parameter id = ${id}`);
     
     fs.readFile(DB_PATH, 'utf8', (err, data) => {
         if (err) {
