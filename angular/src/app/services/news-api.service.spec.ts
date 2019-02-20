f401966d0bdc061e8f652555f57d8865264dd89d
Ihar Shavel <eagle_ig@tut.by>
2019-02-21 02:20:10 +0300

    test: increase code coverage

--- angular/src/app/services/news-api.service.spec.ts
+++ angular/src/app/services/news-api.service.spec.ts
@@ -2,10 +2,13 @@ import { HttpClient } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 
 import { NewsApiService } from "./news-api.service";
+import { of } from "rxjs";
 
 const httpClientMock: any = jasmine.createSpyObj("HttpClient", ["get"]);
 
 describe("ArticeService", () => {
+    let service: NewsApiService;
+
     beforeEach(() =>
         TestBed.configureTestingModule({
             providers: [
@@ -17,8 +20,42 @@ describe("ArticeService", () => {
         })
     );
 
+    beforeEach(() => {
+        httpClientMock.get.and.returnValue(of(undefined));
+        service = TestBed.get(NewsApiService);
+    });
+
     it("should be created", () => {
-        const service: NewsApiService = TestBed.get(NewsApiService);
         expect(service).toBeTruthy();
+    });
+
+    it("getEverything", () => {
+        const responseMock: any = {
+            articles: "fakeArticles"
+        };
+        httpClientMock.get.and.returnValue(of(responseMock));
+        service
+            .getEverything()
+            .subscribe(response => expect(response).toBe(<any>"fakeArticles"));
+    });
+
+    it("getTopHeadlines", () => {
+        const responseMock: any = {
+            articles: "fakeArticles"
+        };
+        httpClientMock.get.and.returnValue(of(responseMock));
+        service
+            .getTopHeadlines()
+            .subscribe(response => expect(response).toBe(<any>"fakeArticles"));
+    });
+
+    it("getSources", () => {
+        const responseMock: any = {
+            sources: "fakeSources"
+        };
+        httpClientMock.get.and.returnValue(of(responseMock));
+        service
+            .getSources()
+            .subscribe(response => expect(response).toBe(<any>"fakeSources"));
     });
 });
--- angular/src/app/services/nodejs-news.service.spec.ts
+++ angular/src/app/services/nodejs-news.service.spec.ts
@@ -3,9 +3,16 @@ import { TestBed } from "@angular/core/testing";
 import { NodejsNewsService } from "./nodejs-news.service";
 import { HttpClient } from "@angular/common/http";
 
-const httpClientMock: any = jasmine.createSpyObj("HttpClient", ["get"]);
+const httpClientMock: any = jasmine.createSpyObj("HttpClient", [
+    "get",
+    "post",
+    "put",
+    "delete"
+]);
 
 describe("NodejsNewsService", () => {
+    let service: NodejsNewsService;
+
     beforeEach(() =>
         TestBed.configureTestingModule({
             providers: [
@@ -17,8 +24,53 @@ describe("NodejsNewsService", () => {
         })
     );
 
+    beforeEach(() => {
+        service = TestBed.get(NodejsNewsService);
+    });
+
     it("should be created", () => {
-        const service: NodejsNewsService = TestBed.get(NodejsNewsService);
         expect(service).toBeTruthy();
+    });
+
+    it("getArticles should perform get request", () => {
+        service.getArticles();
+    });
+
+    it("getArticle should perform get request", () => {
+        const id: string = "fake-id";
+        service.getArticle(id);
+
+        expect(httpClientMock.get).toHaveBeenCalled();
+    });
+
+    it("deleteArticle should perform delete request", () => {
+        const id: string = "fake-id";
+        service.deleteArticle(id);
+
+        expect(httpClientMock.delete).toHaveBeenCalled();
+    });
+
+    it("addArticle should perform post request", () => {
+        const request: any = {};
+        service.addArticle(request);
+
+        expect(httpClientMock.post).toHaveBeenCalled();
+    });
+
+    it("addArticle should perform put request", () => {
+        const request: any = {};
+        service.updateArticle(request);
+
+        expect(httpClientMock.put).toHaveBeenCalled();
+    });
+
+    it("addArticle should perform post request", () => {
+        const logInRequest: any = {
+            login: "login",
+            password: "password"
+        };
+        service.logIn(logInRequest);
+
+        expect(httpClientMock.post).toHaveBeenCalled();
     });
 });
